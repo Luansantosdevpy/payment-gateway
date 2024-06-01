d27de14c68e1c350d84ac385bbc0d39c9b525470
luansantosdevpy <luansantosdev.py@gmail.com>
2024-06-01 16:42:04 -0300

    feat: initial configurations to project

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import 'reflect-metadata';
+import cors from 'cors';
+import express from 'express';
+import { Server } from 'http';
+import { createLogger } from './utils/logger';
+import routes from './routes/routes';
+
+const logger = createLogger("App");
+
+export default class App {
+  public express: express.Application = express();
+
+  private server: Server;
+
+  public initialize = async (): Promise<void> => {
+    await this.middlewares();
+    await this.routes();
+  };
+
+  public start = (port: number, appName: string): void => {
+    this.server = this.express.listen(port, '0.0.0.0', () => {
+        logger.info(`${appName} listening on port ${port}!`);
+    });
+  };
+
+  public stop = (): void => {
+    this.server.close();
+  };
+
+  private middlewares = async (): Promise<void> => {
+    this.express.use(express.json());
+    this.express.use(
+      cors({
+        origin: '*',
+        methods: 'POST, GET, PUT, OPTIONS, PATCH, DELETE',
+        exposedHeaders: 'X-file-name'
+      })
+    );
+    this.express.use(cors());
+  };
+
+  private routes = async (): Promise<void> => {
+    this.express.use(await routes());
+  };
+}
---
+++ src/routes/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+export default async (): Promise<Router> => {
+  const router = Router();
+
+  return router;
+};
