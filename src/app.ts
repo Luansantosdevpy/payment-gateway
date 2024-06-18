4bf9ba36b190408d472d2597eb52a8049be5b500
luansantosdevpy <luansantosdev.py@gmail.com>
2024-06-18 12:19:37 -0300

    feat: change application structure

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,59 @@ import 'reflect-metadata';
 import cors from 'cors';
 import express from 'express';
 import { Server } from 'http';
-import routes from './routes/routes';
-import errorMiddleware from './middlewares/errorMiddleware';
 import Logger from './utils/logger/logger';
+import configureRoutes from './config/routes';
 
 export default class App {
   public express: express.Application = express();
-
   private server!: Server;
 
+  /**
+   * Initializes middlewares and routes
+   */
   public initialize = async (): Promise<void> => {
-    await this.middlewares();
-    await this.routes();
+    try {
+      await this.middlewares();
+      await this.routes();
+    } catch (error) {
+      Logger.error('Error during app initialization', error);
+      process.exit(1);
+    }
   };
 
-  public start = (port: number, appName: string): void => {
+  /**
+   * Starts the server
+   * @param port - Port number to listen on
+   * @param appName - Name of the application
+   */
+  public start = (
+    port: number = parseInt(process.env.PORT || '3000', 10),
+    appName: string,
+  ): void => {
     this.server = this.express.listen(port, '0.0.0.0', () => {
       Logger.info(`${appName} listening on port ${port}!`);
     });
   };
 
+  /**
+   * Stops the server
+   */
   public stop = (): void => {
-    this.server.close();
+    if (this.server) {
+      this.server.close((err) => {
+        if (err) {
+          Logger.error('Error while stopping the server', err);
+        } else {
+          Logger.info('Server stopped successfully');
+        }
+      });
+    }
+    Logger.warn('Server is not running');
   };
 
+  /**
+   * Configures middlewares
+   */
   private middlewares = async (): Promise<void> => {
     this.express.use(express.json());
     this.express.use(
@@ -35,12 +64,12 @@ export default class App {
         exposedHeaders: 'X-file-name',
       }),
     );
-    this.express.use(cors());
   };
 
+  /**
+   * Configures routes and error handling middleware
+   */
   private routes = async (): Promise<void> => {
-    this.express.use(await routes());
-
-    this.express.use(errorMiddleware);
+    await configureRoutes(this.express);
   };
 }
